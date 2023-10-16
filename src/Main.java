dbe4150721cf6be4a7886be88ceef7937e8644a1
Sergei137 <sm1.618@protonmail.com>
2023-10-15 22:23:57 -0600

    small edit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
+
         // Exercise 1 - Sort students by name and age
         System.out.println("\nExercise 1 - Sort students by name and age");
         Exercise1.sortStudents();
@@ -20,7 +21,5 @@ public class Main {
         scanner.close();
         System.out.println("\nProgram terminated.");
         }
-
-        
     }        
 }
